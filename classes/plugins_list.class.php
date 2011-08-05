92dddf00bfb6f30d3ce1e6e1fd1f8e9395c3cc66
Piyush Mishra <me@piyushmishra.com>
2011-08-05 22:59:47 +0530

    check issue #26 different icon for one plugin

--- classes/plugins_list.class.php
+++ classes/plugins_list.class.php
@@ -1,9 +1,20 @@
 <?php
+/**
+ * Plugin Manager plugins list
+ *
+ * @license    GPL 3 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Piyush Mishra <me@piyushmishra.com>
+ */
+
 class plugins_list {
 
     protected $form = null;
     protected $actions = array();
 
+    /**
+     * Plugins list constructor
+     * Starts the form, table and sets up actions available to the user
+     */
     function __construct($id,$actions) {
         $this->actions[''] = '-Please Choose-';
         $this->actions = array_merge($this->actions,$actions);
@@ -14,8 +25,11 @@ class plugins_list {
     }
 
     /**
-     * Build single row of plugin form
-     * @param array checkbox 
+     * Build single row of plugin table
+     * @param string $class    class of the table row
+     * @param array  $info     a single plugin from repo cache
+     * @param string $actions  html for what goes into the action column
+     * @param array  $checkbox the optional parameters to be passed in for the checkbox (use-case disabling downloads)
      */
     function add_row($class,$info,$actions,$checkbox = array()) {
         $this->form->addElement(form_makeOpenTag('tr',array('class'=>$class)));
@@ -40,6 +54,10 @@ class plugins_list {
         $this->form->addElement(form_makeCloseTag('tr'));
     }
 
+    /**
+     * Add closing tags and render the form
+     * @param string $name Name of the event to trigger
+     */
     function render($name = null) {
         $this->form->addElement(form_makeCloseTag('table'));
         $this->form->addElement(form_makeOpenTag('div',array('class'=>'bottom')));
@@ -52,14 +70,30 @@ class plugins_list {
             $this->form->printForm();
     }
 
+    /**
+     * Generate title url for a single plugin
+     * @param array $info a single plugin from repo cache
+     * @return string url or title of the plugin
+     */
     function make_title($info) {
+        $name = hsc($info['name']);
         if(array_key_exists('dokulink',$info) && strlen($info['dokulink'])) {
             $info['url'] = "http://dokuwiki.org/".$info['dokulink'];
-            return '<a href="'.$info['url'].'" title="'.$info['url'].'" class ="interwiki iw_doku" >'.hsc($info['name']).'</a>';
+            return $this->make_link($info,"interwiki iw_doku");
         }
+
         if(array_key_exists('url',$info) && strlen($info['url'])) {
-            return '<a href="'.$info['url'].'" title="'.$info['url'].'" class ="urlextern" >'.hsc($info['name']).'</a>';
+            // not using preg_match as this is pretty much the only case
+            if(stripos('http://dokuwiki.org/',$info['url']) === 0)
+                return $this->make_link($info,"interwiki iw_doku");
+            else
+                return $this->make_link($info,"urlextern");
         }
+
         return  hsc($info['name']);
+    }
+
+    function make_link($info, $class) {
+        return '<a href="'.$info['url'].'" title="'.$info['url'].'" class ="'.$class.'">'.hsc($info['name']).'</a>';
     }
 }
