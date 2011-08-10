69ce748b6bc91eb5ae9681efc630452fc68d05e0
Piyush Mishra <me@piyushmishra.com>
2011-08-10 18:11:26 +0530

    confirm delete bug fix. added preventDefault if not confirmed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var plugin_manager = { 
     constructor : function () {
-        jQuery('#plugin__manager .delete').click(function() {confirm("Are you sure?");});
+        jQuery('#plugin__manager .delete').click(function(e) {if(!confirm("Are you sure?")) { e.preventDefault(); }});
     }
 };
 jQuery(plugin_manager.constructor);
