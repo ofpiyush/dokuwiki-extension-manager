5ebe7ccc21c470d168236af0871d5d106293805f
Hakan Sandell <sandell.hakan@gmail.com>
2012-01-10 19:33:21 +0100

    Don't override info anchor redirect in search view

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var extension_manager = { 
     constructor : function () {
-        jQuery('#extensionplugin__searchtext').focus();
+        if (!extension_manager.getUrlVars()['info']) {
+            jQuery('#extensionplugin__searchtext').focus();
+        }
         extension_manager.setCheckState('#extension__manager .checknone',false);
 	    extension_manager.setCheckState('#extension__manager .checkall',true);
 	    extension_manager.confirmDelete('#extension__manager .actions .delete');
         extension_manager.confirmDelete('#extension__manager .bottom .button[name="fn[delete]"]');
+    },
+    getUrlVars : function() {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
     },
     setCheckState : function (clickSelector,bool) {
         jQuery(clickSelector).show();
