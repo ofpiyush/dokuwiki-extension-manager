8374373eef599bc0b12eadeb0ab83438f9618e84
Andreas Gohr <andi@splitbrain.org>
2012-12-09 17:45:56 +0100

    auto cleanup

--- helper.php
+++ helper.php
@@ -8,7 +8,7 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-if(!defined('DOKU_TPLLIB')) define('DOKU_TPLLIB',DOKU_INC.'lib/tpl/');
+if(!defined('DOKU_TPLLIB')) define('DOKU_TPLLIB', DOKU_INC.'lib/tpl/');
 
 class helper_plugin_extension extends DokuWiki_Plugin {
 
@@ -35,13 +35,13 @@ class helper_plugin_extension extends DokuWiki_Plugin {
     /**
      * array list of bundled plugins
      */
-    var $_bundled = array('acl','plugin','config','info','usermanager','revert','popularity','safefnrecode','template:default');
+    var $_bundled = array('acl', 'plugin', 'config', 'info', 'usermanager', 'revert', 'popularity', 'safefnrecode', 'template:default');
 
     /**
      * plugins that are an integral part of dokuwiki, this is only valid for pre-"Angua" releases
      * now this information is stored in 'conf/plugins.required.php'
      */
-    var $legacy_protected = array('acl','plugin','config','usermanager');
+    var $legacy_protected = array('acl', 'plugin', 'config', 'usermanager');
 
     /**
      * plugins that are protected from being managed with the extension manager
@@ -75,19 +75,21 @@ class helper_plugin_extension extends DokuWiki_Plugin {
      */
     var $dokuwiki_version = null;
 
-    var $dokuwiki = array('2012-01-25' => 'Angua',
-                          '2011-05-25' => 'Rincewind',
-                          '2010-11-07' => 'Anteater',
-                          '2009-12-25' => 'Lemming');
+    var $dokuwiki = array(
+        '2012-01-25' => 'Angua',
+        '2011-05-25' => 'Rincewind',
+        '2010-11-07' => 'Anteater',
+        '2009-12-25' => 'Lemming'
+    );
 
     function __construct() {
-        spl_autoload_register(array($this,'autoload'));
+        spl_autoload_register(array($this, 'autoload'));
     }
 
     function init($manager) {
         $this->manager = $manager;
 
-        if (function_exists('plugin_getcascade')) {
+        if(function_exists('plugin_getcascade')) {
             $cascade = plugin_getcascade();
             if(!empty($cascade['protected'])) {
                 $this->protected = array_keys($cascade['protected']);
@@ -96,37 +98,38 @@ class helper_plugin_extension extends DokuWiki_Plugin {
             // support for using extension manager with pre-"Angua" (okt2011) releases
             $this->protected = $this->legacy_protected;
         }
-        $this->pluginfolder_writable = is_writable(DOKU_PLUGIN);
+        $this->pluginfolder_writable   = is_writable(DOKU_PLUGIN);
         $this->templatefolder_writable = is_writable(DOKU_TPLLIB);
 
         $ver = getVersionData();
-        if (preg_match('/\d+[-]\d+[-]\d+/',$ver['date'],$date)) {
+        if(preg_match('/\d+[-]\d+[-]\d+/', $ver['date'], $date)) {
             reset($this->dokuwiki);
-            if ($ver['type'] == 'Git' && $date[0] > key($this->dokuwiki)) {
+            if($ver['type'] == 'Git' && $date[0] > key($this->dokuwiki)) {
                 $date[0] = key($this->dokuwiki);
             }
             $name = $this->dokuwiki[$date[0]];
-            if (!$name) $name = $date[0];
+            if(!$name) $name = $date[0];
             $this->dokuwiki_version = array('date' => $date[0], 'name' => $name);
         }
 
         $this->get_plugin_list();
         $this->get_template_list();
 
-        $this->log = new pm_log_lib($this);
-        $repo = new pm_repository_lib($this);
+        $this->log  = new pm_log_lib($this);
+        $repo       = new pm_repository_lib($this);
         $this->repo = $repo->get();
         $this->info = new pm_info_lib($this);
     }
 
     /**
      * Autoloader for the plugin manager
+     *
      * @param string classname to load
      */
     function autoload($class) {
-        if(stripos($class,'pm_')===0) {
-            $folder = @end(explode('_',$class));
-            $path = DOKU_PLUGIN.'extension/classes/'.$folder.'/'.$class.".class.php";
+        if(stripos($class, 'pm_') === 0) {
+            $folder = @end(explode('_', $class));
+            $path   = DOKU_PLUGIN.'extension/classes/'.$folder.'/'.$class.".class.php";
             if(@file_exists($path)) {
                 require_once($path);
                 return true;
@@ -137,12 +140,13 @@ class helper_plugin_extension extends DokuWiki_Plugin {
 
     /**
      * Get plugin list
+     *
      * @return array list of plugins, including disabled ones
      */
     private function get_plugin_list() {
-        if (empty($this->plugin_list)) {
-            $list = plugin_list('',true);     // all plugins, including disabled ones
-            trigger_event('PLUGIN_PLUGINMANAGER_PLUGINLIST',$list);
+        if(empty($this->plugin_list)) {
+            $list = plugin_list('', true); // all plugins, including disabled ones
+            trigger_event('PLUGIN_PLUGINMANAGER_PLUGINLIST', $list);
             $this->plugin_list = $list;
         }
         return $this->plugin_list;
@@ -150,12 +154,13 @@ class helper_plugin_extension extends DokuWiki_Plugin {
 
     /**
      * Get template list
+     *
      * @return array list of templates, including disabled ones
      */
     private function get_template_list() {
         if(empty($this->template_list)) {
             $tpl_dir = DOKU_TPLLIB;
-            $list = array();
+            $list    = array();
             if($dh = @opendir($tpl_dir)) {
                 while(false !== ($template = readdir($dh))) {
                     if($template[0] == '.') continue;
@@ -165,7 +170,7 @@ class helper_plugin_extension extends DokuWiki_Plugin {
                     }
                 }
             }
-            trigger_event('PLUGIN_PLUGINMANAGER_TEMPLATELIST',$list);
+            trigger_event('PLUGIN_PLUGINMANAGER_TEMPLATELIST', $list);
             $this->template_list = $list;
         }
         return $this->template_list;
@@ -180,16 +185,19 @@ class helper_plugin_extension extends DokuWiki_Plugin {
         static $plugins;
         if(empty($plugins[$plugin])) {
             $components = array();
-            $path = DOKU_PLUGIN.plugin_directory($plugin).'/';
+            $path       = DOKU_PLUGIN.plugin_directory($plugin).'/';
 
-            foreach ($plugin_types as $type) {
-                if (@file_exists($path.$type.'.php')) { $components[] = array('name'=>$plugin, 'type'=>$type); continue; }
+            foreach($plugin_types as $type) {
+                if(@file_exists($path.$type.'.php')) {
+                    $components[] = array('name' => $plugin, 'type' => $type);
+                    continue;
+                }
 
-                if ($dh = @opendir($path.$type.'/')) {
-                    while (false !== ($cp = readdir($dh))) {
-                        if ($cp == '.' || $cp == '..' || strtolower(substr($cp,-4)) != '.php') continue;
+                if($dh = @opendir($path.$type.'/')) {
+                    while(false !== ($cp = readdir($dh))) {
+                        if($cp == '.' || $cp == '..' || strtolower(substr($cp, -4)) != '.php') continue;
 
-                        $components[] = array('name'=>$plugin.'_'.substr($cp, 0, -4), 'type'=>$type);
+                        $components[] = array('name' => $plugin.'_'.substr($cp, 0, -4), 'type' => $type);
                     }
                     closedir($dh);
                 }
@@ -204,13 +212,17 @@ class helper_plugin_extension extends DokuWiki_Plugin {
      */
     function get_filtered_repo() {
         $retval = array();
-        if ($this->repo) {
-            $retval = array_filter($this->repo['data'],create_function('$info','return $info["show"];'));
+        if($this->repo) {
+            $retval = array_filter($this->repo['data'], create_function('$info', 'return $info["show"];'));
             $retval = array_merge($retval, $this->local_extensions());
         } else {
             $retval = $this->local_extensions();
         }
-        uasort($retval, function($a,$b){return strcasecmp($a['sort'],$b['sort']);});
+        uasort(
+            $retval, function ($a, $b) {
+                return strcasecmp($a['sort'], $b['sort']);
+            }
+        );
         return $retval;
     }
 
@@ -218,45 +230,46 @@ class helper_plugin_extension extends DokuWiki_Plugin {
      * Create dummy repo entries for local extensions
      */
     function local_extensions() {
-        $retval = array();
-        $templates = array_map(array($this,'_info_templatelist') ,$this->template_list);
-        $plugins = array_map(array($this,'_info_pluginlist'),$this->plugin_list);
-        $list = array_merge($plugins,$templates);
-        foreach ($list as $info) {
-            if ($info->repo) {
+        $retval    = array();
+        $templates = array_map(array($this, '_info_templatelist'), $this->template_list);
+        $plugins   = array_map(array($this, '_info_pluginlist'), $this->plugin_list);
+        $list      = array_merge($plugins, $templates);
+        foreach($list as $info) {
+            if($info->repo) {
                 // only use repo if we are sure that this plugin is connected to repo
-                $retval[$info->repokey] = $info->repo;
+                $retval[$info->repokey]       = $info->repo;
                 $retval[$info->repokey]['id'] = $info->cmdkey;
             } else {
-                $retval['L'.$info->repokey] = array('id' => $info->cmdkey,
-                                                'name' => $info->name,
-                                                'author' => $info->author,
-                                                'description' => $info->desc,
-                                                'sort' => str_replace('template:','',$info->repokey)
-                                                );
+                $retval['L'.$info->repokey] = array(
+                    'id'          => $info->cmdkey,
+                    'name'        => $info->name,
+                    'author'      => $info->author,
+                    'description' => $info->desc,
+                    'sort'        => str_replace('template:', '', $info->repokey)
+                );
             }
         }
         return $retval;
     }
 
     function _info_pluginlist($index) {
-        return $this->info->get($index,'plugin');
+        return $this->info->get($index, 'plugin');
     }
 
     function _info_templatelist($index) {
-        return $this->info->get($index,'template');
+        return $this->info->get($index, 'template');
     }
 
     function make_extensionsearchlink($id) {
         global $ID;
 
         $params = array(
-            'do'=>'admin',
-            'page'=>'extension',
-            'tab'=>'search',
-            'q'=>'id:'.$id,
+            'do'   => 'admin',
+            'page' => 'extension',
+            'tab'  => 'search',
+            'q'    => 'id:'.$id,
         );
-        $url = wl($ID,$params);
+        $url    = wl($ID, $params);
         return '<a href="'.$url.'" class="searchlink" title="'.hsc($id).'">'.hsc(ucfirst($id)).'</a>';
     }
 
