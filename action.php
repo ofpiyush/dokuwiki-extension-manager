c700f9d0ab70be634531e19963367fad2d93da5f
Andreas Gohr <andi@splitbrain.org>
2012-12-09 18:21:48 +0100

    auto cleanup

--- action.php
+++ action.php
@@ -3,21 +3,21 @@
  * AJAX handler for extension details query
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Håkan Sandell <sandell.hakan@gmail.com>
+ * @author     Hï¿½kan Sandell <sandell.hakan@gmail.com>
  */
 
 // must be run within Dokuwiki
-if (!defined('DOKU_INC'))
+if(!defined('DOKU_INC'))
     die();
 
 class action_plugin_extension extends DokuWiki_Action_Plugin {
 
-    var $hlp = null;
-    var $handler = null;
+    public $hlp = null;
+    public $handler = null;
 
     function __construct() {
         $this->hlp =& plugin_load('helper', 'extension');
-        if(!$this->hlp) msg('Loading the extension manager helper failed.',-1);
+        if(!$this->hlp) msg('Loading the extension manager helper failed.', -1);
     }
 
     /**
@@ -32,8 +32,8 @@ class action_plugin_extension extends DokuWiki_Action_Plugin {
         $event->preventDefault();
         $event->stopPropagation();
         $this->hlp->init($this);
-        
-        if ($_POST['fn']) {
+
+        if($_POST['fn']) {
             $this->extension_details();
         } else {
             $this->search_extension();
@@ -45,18 +45,18 @@ class action_plugin_extension extends DokuWiki_Action_Plugin {
      */
     function search_extension() {
         $this->query = hsc($_POST['q']);
-        $repo = $this->hlp->get_filtered_repo($_POST['type']);
-        $hits = array_filter($repo,array($this, 'search_check'));
+        $repo        = $this->hlp->get_filtered_repo($_POST['type']);
+        $hits        = array_filter($repo, array($this, 'search_check'));
 
         echo '<strong>'.$this->getLang('matching_extensions').'</strong>';
         echo '<ul>';
-        foreach ($hits as $info) {
-            $id = explode('/',$info["id"]);
+        foreach($hits as $info) {
+            $id = explode('/', $info["id"]);
             echo '<li>'.$this->hlp->make_extensionsearchlink($id[0]).'</li>';
         }
         echo '</ul>';
     }
-    
+
     function search_check($info) {
         return stripos($info["id"], $this->query) !== false;
     }
@@ -67,11 +67,11 @@ class action_plugin_extension extends DokuWiki_Action_Plugin {
      */
     function extension_details() {
         $fn = $_POST['fn'];
-        preg_match('/(?<=\[info\]\[).+[^\]]/',$fn,$repokey);
+        preg_match('/(?<=\[info\]\[).+[^\]]/', $fn, $repokey);
 
-        $info = $this->hlp->info->get($repokey[0]);
+        $info          = $this->hlp->info->get($repokey[0]);
         $this->handler = new pm_plugin_tab($this);
-        $list = new pm_plugins_list_lib($this,'extensionplugin__pluginsinfo');
+        $list          = new pm_plugins_list_lib($this, 'extensionplugin__pluginsinfo');
         echo $list->make_info($info);
     }
 
