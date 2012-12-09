a7ec729f09819d9b671ae39f45e05e78f3ac1e94
Andreas Gohr <andi@splitbrain.org>
2012-12-09 19:04:50 +0100

    auto cleanup

--- classes/tab/pm_search_tab.class.php
+++ classes/tab/pm_search_tab.class.php
@@ -5,40 +5,39 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Piyush Mishra <me@piyushmishra.com>
  */
-
 class pm_search_tab extends pm_base_tab {
 
-    var $query = NULL;
-    var $term = NULL;
-    var $extra = NULL;
+    var $query = null;
+    var $term = null;
+    var $extra = null;
     var $filters = array();
     var $search_result = array();
     var $search_result_type = 'template';
-    var $filtered_repo = NULL;
+    var $filtered_repo = null;
     var $actions_list = array();
 
     function process() {
-        $this->filtered_repo = $this->helper->get_filtered_repo();
-        $this->actions_list = array(
-            'enable'=>$this->manager->getLang('btn_enable'),
-            'disable'=>$this->manager->getLang('btn_disable'),
-            'update'=>$this->manager->getLang('btn_update'),
-            'reinstall' =>$this->manager->getLang('btn_reinstall'),
-            'download'=>$this->manager->getLang('btn_download'),
+        $this->filtered_repo   = $this->helper->get_filtered_repo();
+        $this->actions_list    = array(
+            'enable'              => $this->manager->getLang('btn_enable'),
+            'disable'             => $this->manager->getLang('btn_disable'),
+            'update'              => $this->manager->getLang('btn_update'),
+            'reinstall'           => $this->manager->getLang('btn_reinstall'),
+            'download'            => $this->manager->getLang('btn_download'),
             // TODO 'download_disabled'=>$this->manager->getLang('btn_disdown'),
             'download_dependency' => $this->manager->getLang('btn_dependown'),
-            );
+        );
         $this->possible_errors = array(
-            'needed_by' => $this->manager->getLang('needed_by'),
-            'bundled' => $this->manager->getLang('bundled_source'),
+            'needed_by'     => $this->manager->getLang('needed_by'),
+            'bundled'       => $this->manager->getLang('bundled_source'),
             'has_conflicts' => $this->manager->getLang('conflicts'),
-            'gitmanaged' => $this->manager->getLang('gitmanaged'),
+            'gitmanaged'    => $this->manager->getLang('gitmanaged'),
             'missing_dlurl' => $this->manager->getLang('no_url'),
-            'not_writable' => $this->manager->getLang('not_writable'),
-            );
+            'not_writable'  => $this->manager->getLang('not_writable'),
+        );
 
         // list of properties that are included in the search
-        $this->filters = array('id' => NULL,'name' => NULL,'description' => NULL, 'type' => NULL, 'tag' =>NULL, 'author' => NULL);
+        $this->filters = array('id' => null, 'name' => null, 'description' => null, 'type' => null, 'tag' => null, 'author' => null);
 
         if(!empty($_REQUEST['q'])) {
             $this->query = $_REQUEST['q'];
@@ -47,20 +46,20 @@ class pm_search_tab extends pm_base_tab {
             $this->query .= ' @'.$_REQUEST['type'];
         }
 
-        if (preg_match_all('/(@plugins*|@templates*|\w+\s*:\s*\w+|".*?"|\w+)+/i',$this->query,$matches)) {
-            foreach ($matches[0] as $match) {
-                if (stripos($match,'@plugin') !== false) {
+        if(preg_match_all('/(@plugins*|@templates*|\w+\s*:\s*\w+|".*?"|\w+)+/i', $this->query, $matches)) {
+            foreach($matches[0] as $match) {
+                if(stripos($match, '@plugin') !== false) {
                     $this->extra['is_template'] = false;
 
-                } elseif (stripos($match,'@template') !== false) {
+                } elseif(stripos($match, '@template') !== false) {
                     $this->extra['is_template'] = true;
 
-                } elseif (strpos($match,':') !== false) {
-                    list($key,$val) = explode(':', $match,2);
+                } elseif(strpos($match, ':') !== false) {
+                    list($key, $val) = explode(':', $match, 2);
                     $this->extra[strtolower(trim($key))] = trim($val);
 
-                } elseif (strpos($match,'"') !== false) {
-                    $this->term[] = str_replace('"','',$match);
+                } elseif(strpos($match, '"') !== false) {
+                    $this->term[] = str_replace('"', '', $match);
 
                 } else {
                     $this->term[] = $match;
@@ -68,7 +67,7 @@ class pm_search_tab extends pm_base_tab {
             }
         }
 
-        if($this->term !== null || $this->extra !== null ) {
+        if($this->term !== null || $this->extra !== null) {
             if($this->term === null) $this->term = array();
             if($this->helper->repo)
                 $this->lookup();
@@ -81,8 +80,8 @@ class pm_search_tab extends pm_base_tab {
     function html() {
         $this->html_menu();
         ptln('<div class="panelHeader">');
-        if ($this->helper->repo) {
-            $summary = sprintf($this->manager->getLang('summary_search'),count($this->helper->repo['data']));
+        if($this->helper->repo) {
+            $summary = sprintf($this->manager->getLang('summary_search'), count($this->helper->repo['data']));
             ptln('<h3>'.$summary.'</h3>');
         } else {
             echo '<div class="message error">'.$this->manager->getLang('repocache_error').'</div>';
@@ -95,7 +94,7 @@ class pm_search_tab extends pm_base_tab {
         $this->tagcloud();
         ptln('</div>');
         ptln('<div class="search">');
-        $this->html_search(null,$this->query);
+        $this->html_search(null, $this->query);
         $this->html_urldownload();
         ptln('</div>');
 
@@ -107,9 +106,9 @@ class pm_search_tab extends pm_base_tab {
     function html_extensionlist() {
         if(is_array($this->search_result) && count($this->search_result)) {
 
-            if ($this->search_result['installed']) {
-                $list = new pm_plugins_list_lib($this->manager,'extensionplugin__searchinstalled',$this->actions_list,$this->possible_errors,$this->search_result_type);
-                $list->add_header('installed_extensions',$this->manager->getLang('header_searchinstalled'));
+            if($this->search_result['installed']) {
+                $list = new pm_plugins_list_lib($this->manager, 'extensionplugin__searchinstalled', $this->actions_list, $this->possible_errors, $this->search_result_type);
+                $list->add_header('installed_extensions', $this->manager->getLang('header_searchinstalled'));
                 $list->start_form();
                 foreach($this->search_result['installed'] as $result) {
                     foreach($result as $info) {
@@ -121,9 +120,9 @@ class pm_search_tab extends pm_base_tab {
                 $list->render();
             }
 
-            if ($this->search_result['repo']) {
-                $list = new pm_plugins_list_lib($this->manager,'extensionplugin__searchresult',$this->actions_list,$this->possible_errors,$this->search_result_type);
-                $list->add_header('search_results',sprintf($this->manager->getLang('header_search_results'),hsc($this->query)));
+            if($this->search_result['repo']) {
+                $list = new pm_plugins_list_lib($this->manager, 'extensionplugin__searchresult', $this->actions_list, $this->possible_errors, $this->search_result_type);
+                $list->add_header('search_results', sprintf($this->manager->getLang('header_search_results'), hsc($this->query)));
                 $list->start_form();
                 foreach($this->search_result['repo'] as $result) {
                     foreach($result as $info) {
@@ -136,15 +135,15 @@ class pm_search_tab extends pm_base_tab {
             }
 
         } elseif(!is_null($this->query)) {
-            $no_result = new pm_plugins_list_lib($this->manager,'extensionplugin__noresult');
-            $no_result->add_header('search_results',sprintf($this->manager->getLang('not_found'),hsc($this->query)));
-            $url = wl($ID,array('do'=>'admin','page'=>'plugin','tab'=>'search'));
-            $no_result->add_p(sprintf($this->manager->getLang('no_result'),$url,$url));
+            $no_result = new pm_plugins_list_lib($this->manager, 'extensionplugin__noresult');
+            $no_result->add_header('search_results', sprintf($this->manager->getLang('not_found'), hsc($this->query)));
+            $url = wl($ID, array('do' => 'admin', 'page' => 'plugin', 'tab' => 'search'));
+            $no_result->add_p(sprintf($this->manager->getLang('no_result'), $url, $url));
             $no_result->render();
 
         } else {
-            $full_list = new pm_plugins_list_lib($this->manager,'extensionplugin__browselist',$this->actions_list,$this->possible_errors);
-            $full_list->add_header('search_results',$this->manager->getLang('browse'));
+            $full_list = new pm_plugins_list_lib($this->manager, 'extensionplugin__browselist', $this->actions_list, $this->possible_errors);
+            $full_list->add_header('search_results', $this->manager->getLang('browse'));
             $full_list->start_form();
             foreach($this->filtered_repo as $info) {
                 $info = $this->_info_list($info['id']);
@@ -157,23 +156,23 @@ class pm_search_tab extends pm_base_tab {
 
     function check_writable() {
         if(!$this->helper->templatefolder_writable) {
-            msg($this->manager->getLang('not_writable')." ".DOKU_TPLLIB,-1);
+            msg($this->manager->getLang('not_writable')." ".DOKU_TPLLIB, -1);
         }
         if(!$this->helper->pluginfolder_writable) {
-            msg($this->manager->getLang('not_writable')." ".DOKU_PLUGIN,-1);
+            msg($this->manager->getLang('not_writable')." ".DOKU_PLUGIN, -1);
         }
     }
 
     /**
      * Output plugin tag filter selection (cloud)
      */
-    function tagcloud(){
+    function tagcloud() {
         global $ID;
 
         $tags = $this->helper->repo['cloud'];
-        if (count($tags) > 0) {
+        if(count($tags) > 0) {
             echo '<div class="cloud">'.NL;
-            foreach($tags as $tag => $size){
+            foreach($tags as $tag => $size) {
                 echo $this->html_taglink($tag, 'cl'.$size);
             }
             echo '</div>'.NL;
@@ -184,28 +183,28 @@ class pm_search_tab extends pm_base_tab {
      * Looks up the term in the repository cache according to filters set
      */
     protected function lookup() {
-        foreach ($this->filtered_repo as $single) {
-            if (!$this->check($single)) continue;
+        foreach($this->filtered_repo as $single) {
+            if(!$this->check($single)) continue;
             // search
-            $matches = array_filter($single,array($this,'search'));
+            $matches = array_filter($single, array($this, 'search'));
             if(count($matches)) {
-                $weight = count(array_intersect_key($this->filters,$matches));
+                $weight = count(array_intersect_key($this->filters, $matches));
                 if($weight) {
                     // increase weight for id (repokey) match
-                    foreach ($this->term as  $term) {
-                        if (stripos($single['id'],$term)!==false) {
+                    foreach($this->term as $term) {
+                        if(stripos($single['id'], $term) !== false) {
                             $weight += 5;
-                            if ($single['id'] == $term) $weight += 10;
+                            if($single['id'] == $term) $weight += 10;
                         }
                     }
 
-                    $group = (strpos($single['id'],'/') !== false ? 'installed' : 'repo');
+                    $group                                               = (strpos($single['id'], '/') !== false ? 'installed' : 'repo');
                     $this->search_result[$group][$weight][$single['id']] = $single;
-                    if (substr($single['id'],0,9) != 'template:') $this->search_result_type = 'plugin';
+                    if(substr($single['id'], 0, 9) != 'template:') $this->search_result_type = 'plugin';
                 }
             }
         }
-        foreach ($this->search_result as &$group) {
+        foreach($this->search_result as &$group) {
             krsort($group);
         }
         return;
@@ -216,10 +215,10 @@ class pm_search_tab extends pm_base_tab {
      */
     protected function search($haystack) {
         if(is_array($haystack)) {
-            return (bool) count(array_filter((array)$haystack,array($this,'search')));
+            return (bool) count(array_filter((array) $haystack, array($this, 'search')));
         }
-        foreach ($this->term as $t) {
-            if (@stripos($haystack,$t) === false) return false;
+        foreach($this->term as $t) {
+            if(@stripos($haystack, $t) === false) return false;
         }
         return true;
     }
@@ -230,17 +229,17 @@ class pm_search_tab extends pm_base_tab {
     protected function check($plugin) {
         if(is_array($this->extra)) {
             foreach($this->extra as $key => $value) {
-                if ($key == 'is_template') {
-                    if ($value) {
-                        if (strpos($plugin['id'],'template:') === false) return false;
+                if($key == 'is_template') {
+                    if($value) {
+                        if(strpos($plugin['id'], 'template:') === false) return false;
                     } else {
-                        if (strpos($plugin['id'],'template:') !== false) return false;
+                        if(strpos($plugin['id'], 'template:') !== false) return false;
                     }
 
-                } elseif ($key == 'tag') {
-                    if(@in_array($value,(array)$plugin['tags']['tag']) === false) return false;
+                } elseif($key == 'tag') {
+                    if(@in_array($value, (array) $plugin['tags']['tag']) === false) return false;
 
-                } elseif (!array_key_exists($key,$plugin) || stripos($plugin[$key], $value) === false) {
+                } elseif(!array_key_exists($key, $plugin) || stripos($plugin[$key], $value) === false) {
                     return false;
                 }
             }
--- classes/tab/pm_template_tab.class.php
+++ classes/tab/pm_template_tab.class.php
@@ -5,37 +5,34 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Piyush Mishra <me@piyushmishra.com>
  */
-
 class pm_template_tab extends pm_base_tab {
-
     var $templates = array();
-    var $updates_available;
     var $enabled = array();
     var $tpl_default = array();
     var $actions_list = array();
 
     function process() {
         global $conf;
-        $list = $this->helper->template_list;
+        $list                          = $this->helper->template_list;
         $this->templates['enabled'][0] = $this->_info_list($conf['template']);
-        $disabled = array_diff($list,array($conf['template']));
-        $this->templates['disabled'] = array_map(array($this,'_info_list'),$disabled);
+        $disabled                      = array_diff($list, array($conf['template']));
+        $this->templates['disabled']   = array_map(array($this, '_info_list'), $disabled);
 
-        $this->updates_available = count(array_filter($this->templates['disabled'], create_function('$info','return $info->update_available;')));
-        if ($this->templates['enabled'][0]->update_available) $this->updates_available++;
+        $this->updates_available = count(array_filter($this->templates['disabled'], create_function('$info', 'return $info->update_available;')));
+        if($this->templates['enabled'][0]->update_available) $this->updates_available++;
 
-        usort($this->templates['disabled'],array($this,'_sort'));
-        $this->actions_list = array(
-            'enable'=>$this->manager->getLang('btn_enable'),
-            'delete'=>$this->manager->getLang('btn_delete'),
-            'update'=>$this->manager->getLang('btn_update'),
-            'reinstall' =>$this->manager->getLang('btn_reinstall'),
+        usort($this->templates['disabled'], array($this, '_sort'));
+        $this->actions_list    = array(
+            'enable'    => $this->manager->getLang('btn_enable'),
+            'delete'    => $this->manager->getLang('btn_delete'),
+            'update'    => $this->manager->getLang('btn_update'),
+            'reinstall' => $this->manager->getLang('btn_reinstall'),
         );
         $this->possible_errors = array(
-            'needed_by' => $this->manager->getLang('needed_by'),
-            'not_writable' => $this->manager->getLang('not_writable'),
-            'bundled' => $this->manager->getLang('bundled_source'),
-            'gitmanaged' => $this->manager->getLang('gitmanaged'),
+            'needed_by'     => $this->manager->getLang('needed_by'),
+            'not_writable'  => $this->manager->getLang('not_writable'),
+            'bundled'       => $this->manager->getLang('bundled_source'),
+            'gitmanaged'    => $this->manager->getLang('gitmanaged'),
             'missing_dlurl' => $this->manager->getLang('no_url'),
         );
     }
@@ -46,8 +43,8 @@ class pm_template_tab extends pm_base_tab {
     function html() {
         $this->html_menu();
         ptln('<div class="panelHeader">');
-        $summary = sprintf($this->manager->getLang('summary_template'),count($this->helper->template_list));
-	    ptln('<h3>'.$summary.'</h3>');
+        $summary = sprintf($this->manager->getLang('summary_template'), count($this->helper->template_list));
+        ptln('<h3>'.$summary.'</h3>');
         $this->html_search($this->manager->tab);
         $this->html_download_disabled();
         ptln('</div><!-- panelHeader -->');
@@ -58,24 +55,24 @@ class pm_template_tab extends pm_base_tab {
     }
 
     function html_extensionlist() {
-        $list = new pm_plugins_list_lib($this->manager,'extensionplugin__templateslist',$this->actions_list,$this->possible_errors,'template');
-        $list->add_header('installed_templates',$this->manager->getLang('header_template_installed'));
+        $list = new pm_plugins_list_lib($this->manager, 'extensionplugin__templateslist', $this->actions_list, $this->possible_errors, 'template');
+        $list->add_header('installed_templates', $this->manager->getLang('header_template_installed'));
         $list->start_form();
         if(!empty($this->templates)) {
             foreach($this->templates as $status => $templates) {
-                foreach ($templates as $template) {
+                foreach($templates as $template) {
                     $list->add_row($template);
                     if($status == "enabled") $list->rowadded = false;
                 }
             }
         }
-        $list->end_form(array('update','delete','reinstall'));
+        $list->end_form(array('update', 'delete', 'reinstall'));
         $list->render();
     }
 
     function check_writable() {
         if(!$this->helper->templatefolder_writable) {
-            msg($this->manager->getLang('not_writable')." ".DOKU_TPLLIB,-1);
+            msg($this->manager->getLang('not_writable')." ".DOKU_TPLLIB, -1);
         }
     }
 
